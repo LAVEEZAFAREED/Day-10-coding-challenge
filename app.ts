f38c500267434980c201e3780260825260610c22
LAVEEZAFAREED <162338181+LAVEEZAFAREED@users.noreply.github.com>
2024-04-29 07:23:12 -0700

    Add files via upload

---
+++ app.ts
@@ -0,0 +1,42 @@
+//DAY 10 CODING CHALLENGE.
+//Question 1:Stages of life:Determine a person life stage with an if-else chain.
+let age:number= 25
+if (age < 2 ){
+    console.log("The person is a baby.");
+}  else if(age < 6)   {
+    console.log("The person is a toddler.");
+}  else if(age < 20) {
+    console.log("The person is a teenager.")
+}else if(age < 27)  {
+    console.log("The person is a younster.");
+}  else if(age < 65)   {
+    console.log("The person is an adult.");
+}else{
+    console.log("The person is an elder.");
+}
+
+//Question 2:Favourite fruit:Create an array for your favourite fruits and check if certain fruit are included.
+let favourite_fruits: string[] = ["apples","bananas","cherries"];
+
+if
+(favourite_fruits.includes("bananas"))  {
+    console.log("You really like bananas.");
+}
+if(favourite_fruits.includes("apples"))  {
+    console.log("You really like apples.");
+    }
+if
+(favourite_fruits.includes("cherries"))    {
+    console.log("You really like cherries.");
+} 
+
+//Question 3:Hello admin:Greet user differently,especially 'admin'.
+let usernames: string[] = ["admin","Laiba","Areeba","Bisma","Erum"];
+
+usernames.forEach(username => {
+    if (username === "admin")  {
+        console.log("Hello admin,would you like to see a status report?");
+    } else {
+        console.log(`Hello ${username}, thank you for logging in again.`);
+    }
+});
